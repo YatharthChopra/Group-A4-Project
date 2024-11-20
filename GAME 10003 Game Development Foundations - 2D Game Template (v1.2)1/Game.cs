9b74958cf5e4e2b6932c649073348652acea933b
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-11-19 19:26:10 -0500

    Add Finish boxes
    Adds finish boxes at top of screen, not final colors just adding structure

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
@@ -18,7 +18,7 @@ namespace Game10003
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(600, 1000);
         }
 
         /// <summary>
@@ -26,7 +26,25 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.DarkGray);
+            Draw.LineColor = Color.Black;
+            Draw.Rectangle(0, 0, 20, 80);
 
+            Draw.LineColor = Color.Black;
+            Draw.Rectangle(100, 0, 40, 80);
+
+            Draw.LineColor = Color.Black;
+            Draw.Rectangle(220, 0, 40, 80);
+
+            Draw.LineColor = Color.Black;
+            Draw.Rectangle(340, 0, 40, 80);
+
+            Draw.LineColor = Color.Black;
+            Draw.Rectangle(460, 0, 40, 80);
+
+            Draw.LineColor = Color.Black;
+            Draw.Rectangle(580, 0, 40, 80);
+           
         }
     }
 }
